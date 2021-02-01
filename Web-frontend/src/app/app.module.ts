2001a37804a3a252aff9dbed2bae69eabc29c429
Reepper <axel@e-marceau.com>
2021-02-02 00:16:01 +0100

    [ADD] Ajout du compasant scene 3D pour les modeles

--- Web-frontend/src/app/app.module.ts
+++ Web-frontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule, NbIconModule, NbUserModule, NbMenuModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { Scene3dComponent } from './scene3d/scene3d.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Scene3dComponent
   ],
   imports: [
     BrowserModule,
---
+++ Web-frontend/src/app/scene3d/scene3d.component.ts
@@ -0,0 +1,168 @@
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import * as GLTF from 'gltf-loader';
+
+@Component({
+  selector: 'app-scene3d',
+  templateUrl: './scene3d.component.html',
+  styleUrls: ['./scene3d.component.scss']
+})
+
+export class Scene3dComponent implements OnInit {
+  ngOnInit() {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(800, 800);
+    document.body.appendChild(renderer.domElement);
+    /*
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+    const cube = new THREE.Mesh(geometry, material);
+    */
+    let loader = new GLTF.GLTFLoader();
+    loader.load('../assets/underground_shelter/scene.gltf', 
+    function(gltf){
+        let object1 = gltf.scene.children[0];
+
+        scene.add(gltf.scene);
+        renderer.render(scene, camera);
+    });
+    camera.position.x += 45/180*Math.PI;
+    
+    //scene.add(cube);
+    //cube.rotation.x += 0.5;
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      //cube.rotation.x += 0.01;
+      //cube.rotation.y += 0.01;
+      camera.position.y += 0.01
+
+      renderer.render(scene, camera);
+    };
+    camera.position.z = 5;
+    renderer.render(scene, camera);
+    animate();
+
+/*
+    let scene, camera, hlight, object1;
+    // creation scene et camera
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
+    //const controls = new THREE.OrbitControls();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    //camera = new THREE.PerspectiveCamera( 40, 800 / 600, 0.1, 1000 );
+    camera.rotation.y = 45/180*Math.PI;
+    camera.position.x = 800
+    camera.position.y = 100;
+    camera.rotation.z = 1000;
+
+    hlight = new THREE.AmbientLight(0x404040, 100);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    // chargement du model
+    let loader = new THREE.GLTFLoader();
+    loader.load('../assets/underground_shelter/scene.gltf', 
+    function(gltf){
+        object1 = gltf.scene.children[0];
+
+        scene.add(gltf.scene);
+        renderer.render(scene, camera);
+    });*/
+  }
+}
+
+/*
+export class Scene3dComponent implements AfterViewInit {
+
+  // propriété de l'env
+  private camera: THREE.PerspectiveCamera;
+
+    // element canvas
+  private get canvas(): HTMLCanvasElement{
+    return this.canvasRef.nativeElement;
+  }
+
+  @ViewChild('canvas')
+  private canvasRef: ElementRef;
+  private mesh: THREE.Mesh;
+  private renderer: THREE.WebGLRenderer;
+  private scene: THREE.Scene;
+
+// propriété de l'objet
+  @Input()
+  public rotationSpeedX: number = 0.005;
+
+  @Input()
+  public rotationSpeedY: number = 0.01;
+
+  @Input()
+  public size: number = 0.005;
+
+  @Input()
+  public texture: string = '../../assets/textures/nyancat.gif';
+
+// propriété de la scene
+  @Input()
+  public cameraZ: number = 400;
+
+  @Input()
+  public fov: number = 70;
+
+  @Input()
+  public nearClippingPane: number = 1;
+
+  @Input()
+  public farClippingPane: number = 1000;
+
+// propriété d'animation
+  private animationMesh(){
+    this.mesh.rotation.x += this.rotationSpeedX;
+    this.mesh.rotation.y += this.rotationSpeedY;
+  }
+
+// création de la scene
+  private createScene(){
+    // scene
+    this.scene = new THREE.Scene();
+
+    // caméra
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fov,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+    this.camera.position.z = this.cameraZ;
+  }
+
+  private getAspectRatio() {
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+// rendu en boucle
+  private startRenderingLoop(){
+    // renderer | utilise l'élement canvas pour le rendu
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas })
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth/100, this.canvas.clientHeight/100);
+
+    let component: Scene3dComponent = this;
+    (function render(){
+      requestAnimationFrame(render);
+      component.animationMesh();
+      component.renderer.render(component.scene, component.camera);
+    })
+  }
+
+  constructor() { }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}*/
